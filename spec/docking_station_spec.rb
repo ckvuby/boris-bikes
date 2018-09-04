f678a1ace27c24f077039623a06e1463ade18923
Vu Le <core.safecrlf>
2018-09-04 17:25:52 +0100

    upto chapter 10, cross file intergration working.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,17 @@
 require "docking_station"
 
 describe DockingStation do
+
   it 'responds to calling "release_bike"' do
     expect(subject).to respond_to(:release_bike)
   end
+
+  describe "release_bike" do
+
+    it "returns a Bike object in a working? state." do
+      expect(subject.release_bike.working?).to eq true
+    end
+
+  end
+
 end
