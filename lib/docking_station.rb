2ff8ec2c0cdc9fc6539dae0b2b4f11d26bc2576c
Vu Le <core.safecrlf>
2018-09-05 14:57:38 +0100

    added 2 features, dock_bike and bike_id

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,17 @@
 require_relative "./bike.rb"
 
 class DockingStation
+  attr_reader :bike_id
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+     @bike_id = bike
+  end
+
+  # def bike_list
+  #   @bike
+  # end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,4 @@
+
 require "docking_station"
 
 describe DockingStation do
@@ -6,7 +7,15 @@ describe DockingStation do
     expect(subject).to respond_to(:release_bike)
   end
 
-  describe "release_bike" do
+  it 'responds to calling "dock_bike"' do
+    expect(subject).to respond_to(:dock_bike).with(1).argument
+  end
+
+  it 'responds to calling bike_list' do
+    expect(subject).to respond_to(:bike_id)
+  end
+
+describe "release_bike" do
 
     it "returns a Bike object in a working? state." do
       expect(subject.release_bike.working?).to eq true
@@ -14,4 +23,21 @@ describe DockingStation do
 
   end
 
+  describe "dock_bike(bike)" do
+
+    it "docks a bike" do
+    bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq(bike)
+    end
+  end
+
+  describe "bike_id" do
+
+    it "returns list of bikes stored" do
+    bike = Bike.new
+    subject.dock_bike(bike)
+      expect(subject.bike_id).to eq(bike)
+    end
+  end
+    
 end
